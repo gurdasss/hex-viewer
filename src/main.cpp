86676e84474f6e11a12402feca8df7028fb98203
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-07-17 17:00:07 +0530

    got to figure out how to read 16 bytes only

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <fstream> // for std::ifstream
+#include <string>  // for std::getline, std::string
+#include <cstddef> // for std::size_t
 
-int main()
+int main(int argc, const char *argv[])
 {
+    if (argc <= 1)
+    {
+        std::cerr << "error: need a text file to read.\n";
+        std::cerr << "hexviewer FILEPATH\n";
+        return 1;
+    }
+
+    std::ifstream in{argv[1]};
+
+    if (!in)
+    {
+        std::cerr << "Could'nt open the given file.\n";
+        return 1;
+    }
+
+    constexpr std::size_t charPerLine{16UL};
+
+    // Value-initializes all elements to zero ('/0')
+    char buffer[charPerLine]{};
+
+    while (in.getline(buffer, charPerLine))
+    {
+        std::cout << in.gcount() << ' ';
+        std::cout << buffer << '\n';
+        // std::cout << "PEEK: " << static_cast<int>(buffer[15UL]);
+    }
+
+    std::cout << in.fail() << '\n';
+
+    // ifstream object (in) will automatically close the file
+    // via its destructor.
 
     return 0;
 }
