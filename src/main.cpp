8edaf38c0328f5ff2eeae90d19ad56f3410dc0a7
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-07-19 19:24:03 +0530

    simplified the extraction and printing process

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> // for std::ifstream
 #include <cstddef> // for std::size_t
+#include <string>
 
 int main(int argc, const char *argv[])
 {
@@ -19,6 +20,36 @@ int main(int argc, const char *argv[])
         return 1;
     }
 
+    constexpr std::streamsize bufLen{16L};
+    char buffer[bufLen]{};
+
+    // read the given file 16-byte at a time
+    // and print them in a group of 16 bytes
+    // on each line
+    // keep the loop running until !(std::ifstream::fail())
+    while (in)
+    {
+        // read bufLen byte(s) at a time
+        in.read(buffer, bufLen);
+
+        // ONLY print the first in.gcount()
+        // chars
+        for (auto i{0}; i < in.gcount(); ++i)
+        {
+            char byte{buffer[i]};
+
+            // substitute un-printable chars
+            // with '.' char
+            if (byte <= 31 || byte >= 127)
+                std::cout << '.';
+            else
+                std::cout << byte;
+        }
+
+        std::cout << '\n';
+    }
+
+#if 0
     // read the given file byte-by-byte
     // and print them in a group of 16 bytes
     // on each line
@@ -62,7 +93,7 @@ int main(int argc, const char *argv[])
         ++charPrintedCount;
     }
 
-    std::cout << '\n';
+#endif
 
     // ifstream object (in) will automatically close the file
     // via its destructor.
